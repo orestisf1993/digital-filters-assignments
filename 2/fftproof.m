50961bf8aa7c981ae54ee9af48ca0d4ce888c0d3
orestisf <orestisf1993@gmail.com>
2016-04-28 19:14:25 +0300

    Improve fftproof.m's formatting
    - 4 space indentation.
    - Sections.
    - ys -> y_correct.
    - Spaces between operators.

--- 2/fftproof.m
+++ 2/fftproof.m
@@ -7,64 +7,65 @@
 % Sept 1992
 %
 
-clear
+%% Init
 
-n = 8; % any even
+clear
+n = 2 ^ 10; % n is power of 2.
 
 % input
-x = randn(n,1) + 1i*randn(n,1);
-% correct answer
-ys = fft(x);
+x = randn(n, 1) + 1i * randn(n, 1);
+y_correct = fft(x);
+
+%% DFT
 
 % root of unity
-w = @(n,e) exp(-2*pi*1i.*e/n);
+w = @(n, e) exp(-2 * pi * 1i .* e / n);
 
-k = (0:n-1)';
+k = (0:n - 1)';
 
 % DFT proof steps
-y = zeros(n,1);
+y = zeros(n, 1);
 
-for j = 0:n-1
-  y(j +1) = sum(w(n,j*k) .* x(k +1));
+for j = 0:n - 1
+    y(j + 1) = sum(w(n, j * k) .* x(k + 1));
 end
 
-fprintf('DFT : %e\n', norm(y - ys))
+fprintf('DFT : %e\n', norm(y - y_correct))
 
-% split output top bottom
-y = zeros(n,1);
+%% Split output top bottom
+y = zeros(n, 1);
 
-for j = 0:n/2-1
-  y(j +1) = sum(w(n,j*k) .* x(k +1));
+for j = 0:n / 2 - 1
+    y(j + 1) = sum(w(n, j * k) .* x(k + 1));
 end
-for j = n/2:n-1
-  y(j +1) = sum(w(n,j*k) .* x(k +1));
-end
-
-fprintf('split output top bottom : %e\n', norm(y - ys))
-
-% split input even odd
-y = zeros(n,1);
-
-k = (0:n/2-1)';
-for j = 0:n/2-1
-  y(j +1) = sum(w(n,j*2*k) .* x(2*k +1)) + sum(w(n,j*(2*k+1)) .* x(2*k+1 +1));
-end
-for j = n/2:n-1
-  y(j +1) = sum(w(n,j*2*k) .* x(2*k +1)) + sum(w(n,j*(2*k+1)) .* x(2*k+1 +1));
+for j = n / 2:n - 1
+    y(j + 1) = sum(w(n, j * k) .* x(k + 1));
 end
 
-fprintf('split input even odd : %e\n', norm(y - ys))
+fprintf('split output top bottom : %e\n', norm(y - y_correct))
 
-% apply w identities
+%% Split input even odd
+y = zeros(n, 1);
+
+k = (0:n / 2 - 1)';
+for j = 0:n / 2 - 1
+    y(j + 1) = sum(w(n, j * 2 * k) .* x(2 * k + 1)) + sum(w(n, j * (2 * k + 1)) .* x(2 * k + 1 + 1));
+end
+for j = n / 2:n - 1
+    y(j + 1) = sum(w(n, j * 2 * k) .* x(2 * k + 1)) + sum(w(n, j * (2 * k + 1)) .* x(2 * k + 1 + 1));
+end
+
+fprintf('split input even odd : %e\n', norm(y - y_correct))
+
+%% Apply w identities
 % etc
 % ...
 
-% to complete the proof
-fe = fft(x((0:2:n-1) +1));
-fo = fft(x((1:2:n-1) +1));
+%% Complete the proof
+fe = fft(x((0:2:n - 1) + 1));
+fo = fft(x((1:2:n - 1) + 1));
 
-wfo = w(n,(0:n/2-1)') .* fo;
+wfo = w(n, (0:n / 2 - 1)') .* fo;
 y = [fe + wfo; fe - wfo];
 
-fprintf('done : %e\n', norm(y - ys))
-
+fprintf('done : %e\n', norm(y - y_correct))
