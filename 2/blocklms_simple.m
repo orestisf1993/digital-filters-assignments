a285ec32db745e8646f03283feb73d99030c62a1
orestisf <orestisf1993@gmail.com>
2016-05-04 00:33:52 +0300

    Update blocklms_simple's doc & whitespace

--- 2/blocklms_simple.m
+++ 2/blocklms_simple.m
@@ -1,14 +1,19 @@
 function [w, J] = blocklms_simple(u, d)
+%BLOCKLMS_SIMPLE Block LMS algorithm using 2 nested loops.
+%   BLOCKLMS_SIMPLE(U, D) will execute the Least Mean Square (LMS) Algorithm for
+%   the input vector U and the desired response D. The following values are
+%   used:
+%       filter length M = 3000
+%       block size L = M
+%       step mu = 2.0e-04
 
 % Esure that u and d are column vectors.
 u = u(:);
 d = d(:);
 
 mu = 2.0e-04;
-% TODO: M, L should be equal and this is correct. BUT which is which? Where
-% should I use M and where should I use L?
-M = 3000;  % number of coefficients.
-L = M;  % block length
+M = 3000;
+L = M;
 w = zeros(M, 1);
 N = length(u);
 
@@ -17,13 +22,13 @@ kmax = floor(N / L) - 1;
 J = zeros(1, kmax * L);
 for k = 1:kmax
     phi = zeros(M, 1);
-    for i = 0:L-1
-        idx = k*L + i:-1:k*L + i - M + 1;
+    for i = 0:L - 1
+        idx = k * L + i:-1:k * L + i - M + 1;
         newu = u(idx);
-        newd = d(idx(1));  % Only one element?
+        newd = d(idx(1));
         y = w'*newu;
         newe = newd - y;
-        J((k-1) * L + i + 1) = newe.^2;
+        J((k - 1) * L + i + 1) = newe .^ 2;
         phi = phi + mu * newe * newu;
     end
     w = w + phi;
