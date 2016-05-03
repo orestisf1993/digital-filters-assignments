b55a2ecf64be14883dbe73b392160513ed7a114c
orestisf <orestisf1993@gmail.com>
2016-05-04 00:35:36 +0300

    Update blocklms_array's doc & whitespace

--- 2/blocklms_array.m
+++ 2/blocklms_array.m
@@ -1,24 +1,31 @@
 function [w, J] = blocklms_array(u, d)
+%BLOCKLMS_ARRAY Block LMS algorithm using a single loop and matrix operations.
+%   BLOCKLMS_ARRAY(U, D) will execute the Least Mean Square (LMS) Algorithm for
+%   the input vector U and the desired response D. The following values are
+%   used:
+%       filter length M = 3000
+%       block size L = M
+%       step mu = 2.0e-04
 
 % Esure that u and d are column vectors.
 u = u(:);
 d = d(:);
 
 mu = 2.0e-04;
-M = 3000;  % number of coefficients.
-L = M;  % block length
+M = 3000;
+L = M;
 w = zeros(M, 1);
 N = length(u);
 
 % Iterate for k block index
-kmax = floor(N / L);  % round? floor? ceil?
+kmax = floor(N / L) - 1;
 J = zeros(1, kmax * L);
-for k = 1:kmax-1
-    idx = repmat(k*L:-1:k*L - M + 1, L, 1) + repmat((0:L-1).', 1, M);
+for k = 1:kmax
+    idx = repmat(k * L:-1:k * L - M + 1, L, 1) + repmat((0:L - 1).', 1, M);
     newu = u(idx);
     newd = d(idx(:, 1));
-    newe = newd - newu*w;
-    save_range = (k-1) * L + 1:k * L;
-    J(save_range) = newe.^2;
+    newe = newd - newu * w;
+    save_range = (k - 1) * L + 1:k * L;
+    J(save_range) = newe .^ 2;
     w = w + mu * (newe.' * newu).';
 end
