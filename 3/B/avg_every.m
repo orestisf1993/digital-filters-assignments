63eebe1fe8d8b39d4477e5a2762dec52978ee8c9
orestisf <orestisf1993@gmail.com>
2016-06-07 01:31:24 +0300

    Write avg_every.m doc

--- 3/B/avg_every.m
+++ 3/B/avg_every.m
@@ -1,13 +1,10 @@
 function [y, x] = avg_every(a, n)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%AVG_EVERY Average every N elements of a vector.
+%   [Y, X] = AVG_EVERY(A, N) will calculate the averaged vector Y for every N
+%   elements of vector A. X is the index of the new vector Y according to the
+%   old index.
 
-if ~exist('n', 'var')
-    % average every n values
-    n = 3000;
-end
-
-x = 1:n:length(a)-n+1;
-y = arrayfun(@(i) mean(a(i:i+n-1)), x); % the averaged vector
+x = 1:n:length(a) - n + 1;
+y = arrayfun(@(i) mean(a(i:i + n - 1)), x); % the averaged vector
 
 end
